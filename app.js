d2afb42dc7e552b0b7a10fd31ad4da4955d4c15e
jaypee <jaypeeig@gmail.com>
2017-01-27 16:58:30 +0800

    Filter and Map function

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 
 /*understanding map function*/
 
-var keys = [1,2,3];
+var keys = [1,2,3,4];
 
 console.log(keys);
+
 
 var newkeys = keys.map(function(key) {
 	return key * 2;
 });
 
-console.log(newkeys);
+//console.log(newkeys);
+
+/*understanding filter function*/
+
+/*returns a boolean, and can be chained to a map function*/
+
+var newkeys = keys.filter(function(key){
+    return (key % 2 !== 0); //odd number
+}).map(function(number){
+    return number * 2;
+});
+
+console.log(newkeys); 
